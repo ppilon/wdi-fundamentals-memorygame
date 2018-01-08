814b7b5d955c0470b7da0d2196e59a05172e8735
Phil Pilon <pilon24@icloud.com>
2018-01-08 11:41:40 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var cardOne = "queen";
+
+var cardTwo = "king";
+
+var cardThree = "queen";
+
+var cardFour =   "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
