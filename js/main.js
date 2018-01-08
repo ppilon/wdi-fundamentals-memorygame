6ed49cab4741697f390800151705670cf0c42d74
Phil Pilon <pilon24@icloud.com>
2018-01-08 11:49:19 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-var cardOne = "queen";
+var cards = ['queen', 'queen', 'king', 'king'];
 
-var cardTwo = "king";
+var cardsInPlay = [];
 
-var cardThree = "queen";
+var cardOne = cards[0];
 
-var cardFour =   "king";
+cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}else{
+		alert('Sorry, try again')
+	}
+}
