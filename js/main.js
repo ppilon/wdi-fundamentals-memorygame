0caa3110452af8a14e664bb2f01d1c8fd5c1b2f4
Phil Pilon <pilon24@icloud.com>
2018-01-09 19:51:05 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -14,11 +32,13 @@ function checkForMatch(){
 
 function flipCard(cardId){
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch();
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 flipCard(0);
